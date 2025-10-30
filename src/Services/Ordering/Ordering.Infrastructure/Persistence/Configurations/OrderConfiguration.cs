705831177a59712a35ea714650b96df195699464
yedil <yedilmyrzabek@gmail.com>
2025-10-30 23:04:18 +0500

    Adding Configuration file to Order Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> e)
+    {
+        e.HasKey(x => x.Id);
+        e.Property(x => x.Number).IsRequired().HasMaxLength(64);
+        e.Property(x => x.CustomerName).IsRequired().HasMaxLength(200);
+        e.Property(x => x.TotalAmount).HasColumnType("numeric(18,2)");
+        e.HasIndex(x => x.Number).IsUnique();
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderingDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Infrastructure.Data;
@@ -8,15 +9,9 @@ public class OrderingDbContext : DbContext
     public OrderingDbContext(DbContextOptions<OrderingDbContext> options) : base(options) { }
     public DbSet<Order> Orders { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder b)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        b.Entity<Order>(e =>
-        {
-            e.HasKey(x => x.Id);
-            e.Property(x => x.Number).IsRequired().HasMaxLength(64);
-            e.Property(x => x.CustomerName).IsRequired().HasMaxLength(200);
-            e.Property(x => x.TotalAmount).HasColumnType("numeric(18,2)");
-            e.HasIndex(x => x.Number).IsUnique();
-        });
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
     }
 }
